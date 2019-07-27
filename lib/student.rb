88199a30f04e2222a629727e30d18851d99cb79d
Candice Parker <lexus2424@gmail.com>
2019-07-27 20:09:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,9 +19,8 @@ class Student
   end 
   
   def self.drop_table
-   sql = DROP TABLE students;
-   DB[:conn].execute(sql)
-    
+    sql = DROP TABLE students;
+    DB[:conn].execute(sql)
   end
 
    
