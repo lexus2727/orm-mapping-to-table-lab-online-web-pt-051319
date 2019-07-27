711cccf048eabb0ab3b82f2680ee7f1498830c74
Candice Parker <lexus2424@gmail.com>
2019-07-27 20:01:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,14 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def  initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  
 
    
   
@@ -14,3 +24,5 @@ end
 
 
 
+ # Remember, you can access your database connection anywhere in this class
+  #  with DB[:conn]
